6d97356a95f634a4f85befeb30b1b7c7074f7abe
chetan-anand <chetan.iitg@gmail.com>
2014-09-15 02:21:24 +0530

    pratice

--- ROTATION.cpp
+++ ROTATION.cpp
@@ -17,21 +17,44 @@ typedef pair <int,int> pii;
 #define fd(i,n,a) for(int i=n;i>=a;i--)
 #define input(f) freopen("f.txt","r",stdin)
 
+inline void fastRead_int(int &x) {
+    register int c = getchar_unlocked();
+    x = 0;
+    int neg = 0;
+
+    for(; ((c<48 || c>57) && c != '-'); c = getchar_unlocked());
+
+    if(c=='-') {
+    	neg = 1;
+    	c = getchar_unlocked();
+    }
+
+    for(; c>47 && c<58 ; c = getchar_unlocked()) {
+    	x = (x<<1) + (x<<3) + c - 48;
+    }
+
+    if(neg)
+    	x = -x;
+}
 int main()
 {
     //freopen("i.txt","r",stdin);
     int n,m,d,i,j,k;
     int a[110000];
-    cin>>n>>m;
+    //cin>>n>>m;
+    fastRead_int(n);
+    fastRead_int(m);
     rep(i,n)
     {
-        cin>>a[i];
+        //cin>>a[i];
+        fastRead_int(a[i]);
     }
     long long int shift=0;
     while(m--)
     {
         char ch;
-        cin>>ch>>d;
+        cin>>ch;
+        fastRead_int(d);
         if(ch=='R')
         {
             int idx=d-1-shift;
