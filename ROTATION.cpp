9292491c25327c5da929763e3f37a3588cfa102e
chetan-anand <chetan.iitg@gmail.com>
2014-09-15 02:16:38 +0530

    pratice

--- ROTATION.cpp
+++ ROTATION.cpp
@@ -1,31 +1,56 @@
-#include <iostream>
-#include <string>
 #include<bits/stdc++.h>
 using namespace std;
+typedef long long ll;
+typedef unsigned long long llu;
+typedef vector <int> vi;
+typedef pair <int,int> pii;
+#define pb push_back
+#define mp make_pair
+#define gi(n) scanf("%d",&n)
+#define gl(n) scanf("%lld",&n)
+#define gs(n) scanf("%s",n);
+#define pi(n) printf("%d\n",n)
+#define pl(n) printf("%lld\n",n)
+#define ps(n) printf("%s\n",n);
+#define rep(i,n) for(int i=0;i<n;i++)
+#define fi(i,a,n) for(int i=a;i<=n;i++)
+#define fd(i,n,a) for(int i=n;i>=a;i--)
+#define input(f) freopen("f.txt","r",stdin)
 
-int main(){
-    freopen("i.txt","r",stdin);
-	int n; cin>>n;
-	int m; cin>>m;
-	vector<int> vec;
-	vec.resize(n+1);
-	for(int i=1;i<=n;i++){
-		cin>>vec[i];
-	}
-	int c=0,temp;
-	char ch;
-	while(m--){
-		cin>>ch>>temp;
-		if(ch=='R'){
-			int x=temp-c;
-			if(x<=0)cout<<vec[x+n]<<endl;
-			else cout<<vec[x]<<endl;
-		}
-		else if(ch=='A'){
-			c=(c+temp)%n;
-		}
-		else{
-			c=(c+(n-temp))%n;
-		}
-	}
+int main()
+{
+    //freopen("i.txt","r",stdin);
+    int n,m,d,i,j,k;
+    int a[110000];
+    cin>>n>>m;
+    rep(i,n)
+    {
+        cin>>a[i];
+    }
+    long long int shift=0;
+    while(m--)
+    {
+        char ch;
+        cin>>ch>>d;
+        if(ch=='R')
+        {
+            int idx=d-1-shift;
+            if(idx<0)
+                cout<<a[idx+n]<<endl;
+            else
+                cout<<a[idx]<<endl;
+        }
+        else if(ch=='C')
+        {
+            shift=(shift+n-d)%n;
+        }
+        else
+        {
+            shift=(shift+d)%n;
+        }
+    }
+
+    //fclose(stdin);
+	return 0;
 }
+
