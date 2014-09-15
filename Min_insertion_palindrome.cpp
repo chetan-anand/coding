cf0bee80e821264249f53cccff0cb4ac5073f137
chetan-anand <chetan.iitg@gmail.com>
2014-09-15 16:30:05 +0530

    pratice

--- Min_insertion_palindrome.cpp
+++ Min_insertion_palindrome.cpp
@@ -1,32 +1,37 @@
-// A Naive recursive program to find minimum number insertions
-// needed to make a string palindrome
+// A Dynamic Programming based program to find minimum number
+// insertions needed to make a string palindrome
 #include <stdio.h>
-#include <limits.h>
 #include <string.h>
 
-// A utility function to find minimum of two numbers
+// A utility function to find minimum of two integers
 int min(int a, int b)
-{  return a < b ? a : b; }
+{   return a < b ? a : b;  }
 
-// Recursive function to find minimum number of insersions
-int findMinInsertions(char str[], int l, int h)
+// A DP function to find minimum number of insersions
+int findMinInsertionsDP(char str[], int n)
 {
-    // Base Cases
-    //if (l > h) return INT_MAX;
-    if (l == h) return 0;
-    if (l == h - 1) return (str[l] == str[h])? 0 : 1;
+    // Create a table of size n*n. table[i][j] will store
+    // minumum number of insertions needed to convert str[i..j]
+    // to a palindrome.
+    int table[n][n], l, h, gap;
 
-    // Check if the first and last characters are same. On the basis of the
-    // comparison result, decide which subrpoblem(s) to call
-    return (str[l] == str[h])? findMinInsertions(str, l + 1, h - 1):
-                               (min(findMinInsertions(str, l, h - 1),
-                                   findMinInsertions(str, l + 1, h)) + 1);
+    // Initialize all table entries as 0
+    memset(table, 0, sizeof(table));
+
+    // Fill the table
+    for (gap = 1; gap < n; ++gap)
+        for (l = 0, h = gap; h < n; ++l, ++h)
+            table[l][h] = (str[l] == str[h])? table[l+1][h-1] :
+                          (min(table[l][h-1], table[l+1][h]) + 1);
+
+    // Return minimum number of insertions for str[0..n-1]
+    return table[0][n-1];
 }
 
-// Driver program to test above functions
+// Driver program to test above function.
 int main()
 {
     char str[] = "geeks";
-    printf("%d", findMinInsertions(str, 0, strlen(str)-1));
+    printf("%d", findMinInsertionsDP(str, strlen(str)));
     return 0;
 }
