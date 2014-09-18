7728c773d76a7b821661c6b44f2f3eaefe424b9f
chetan-anand <chetan.iitg@gmail.com>
2014-09-18 20:52:59 +0530

    pratice

---
+++ FLOORI.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long llu;
+typedef vector <int> vi;
+typedef pair <int,int> pii;
+#define pb push_back
+#define mp make_pair
+#define gi(n) scanf("%d",&n)
+#define gl(n) scanf("%lld",&n)
+#define gs(n) scanf("%s",n);
+#define pi(n) printf("%d\n",n)
+#define pl(n) printf("%lld\n",n)
+#define ps(n) printf("%s\n",n);
+#define rep(i,n) for(int i=0;i<n;i++)
+#define fi(i,a,n) for(int i=a;i<=n;i++)
+#define fd(i,n,a) for(int i=n;i>=a;i--)
+#define input(f) freopen("f.txt","r",stdin)
+
+
+int main()
+{
+    //freopen("i.txt","r",stdin);
+	int t,i,j,k,n,m;
+	cin>>t;
+	while(t--)
+	{
+		cin>>n>>m;
+		fi(i,1,n)
+		{
+			if(i==1){}
+		}
+	}
+    //fclose(stdin);
+	return 0;
+}
