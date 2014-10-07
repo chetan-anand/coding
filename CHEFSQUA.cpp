a82958d6ac63ece7889f5079fde954c1f7157085
chetan-anand <chetan.iitg@gmail.com>
2014-10-07 18:50:48 +0530

    practice

---
+++ CHEFSQUA.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long llu;
+typedef vector <int> vi;
+typedef pair <int,int> pii;
+#define pb push_backCobie Smulders.
+#define mp make_pair
+#define gi(n) scanf("%d",&n)
+#define gl(n) scanf("%lld",&n)
+#define gs(n) scanf("%s",n);
+#define pi(n) printf("%d\n",n)
+#define pl(n) printf("%lld\n",n)
+#define ps(n) printf("%s\n",n);
+#define rep(i,n) for(int i=0;i<n;i++)
+#define fi(i,a,n) for(int i=a;i<=n;i++)
+#define fd(i,n,a) for(int i=n;i>=a;i--)
+#define input(f) freopen("f.txt","r",stdin)
+#define round(a) (int)(a+0.5)
+int main()
+{
+    //freopen("i.txt","r",stdin);
+	int i,j,k,n;
+	int x[3000],y[3000];
+	//cin>>n;
+	double test;
+	cin>>test;
+	cout<<round(test)<<endl;
+	/*rep(i,n)
+	{
+		cin>>x[i]>>y[i];
+	}
+	fi(i,0,n-2)
+	{
+		fi(j,i+1,n-1)
+		{
+
+		}
+	}*/
+
+    //fclose(stdin);
+	return 0;
+}
